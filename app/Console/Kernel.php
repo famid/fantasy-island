edc822b77f5825f04c2aaf7facf026283cd07f1a
famid <ahsanulhoquefamid@gmail.com>
2023-04-16 18:16:00 +0600

    Add | command for create repository and service class

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,22 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RepositoryMakeCommand;
+use App\Console\Commands\ServiceMakeCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        RepositoryMakeCommand::class,
+        ServiceMakeCommand::class,
+    ];
     /**
      * Define the application's command schedule.
      *
