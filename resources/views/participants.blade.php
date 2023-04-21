c057900027b5368b76804cc2092d4975cbdc0e10
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2023-04-21 21:23:08 +0600

    participants

---
+++ resources/views/participants.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class=''>
+
+        </div>
+        @viteReactRefresh
+
+        @vite(['resources/js/download-ticket/main.jsx'])
+    </div>
+@endsection
+
+
+
