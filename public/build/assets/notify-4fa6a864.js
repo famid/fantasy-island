dc778589fac8e20ae84eb4006535307e71a0fe36
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2023-04-26 18:56:30 +0600

    build file

---
+++ public/build/assets/notify-4fa6a864.js
@@ -0,0 +1,1 @@
+import{Q as o}from"./ReactToastify-2767fbe6.js";const t=e=>o(e,{position:"top-center",autoClose:2e3,hideProgressBar:!1,closeOnClick:!0,pauseOnHover:!0,draggable:!0,progress:void 0,theme:"light"});export{t as n};
